934bd3b0dcbd665d4265418bc9fa2a782a1188f7
Devan Samineedi <dsamineedi@splunk.com>
2019-12-10 14:18:05 -0800

    Intial commit for the grocerylist app

---
+++ GroceryList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GroceryList
+//
+//  Created by Devan Samineedi on 12/10/19.
+//  Copyright © 2019 Devan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
